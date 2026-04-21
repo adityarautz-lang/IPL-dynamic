51f40d183083c1b16bdcaa0d847eefa566c990bb
Aditya Raut <adityarautz@gmail.com>
2026-04-21 13:07:53 +0530

    fix daily chart height rendering

--- app/components/DailyChart.tsx
+++ app/components/DailyChart.tsx
@@ -117,7 +117,8 @@ export default function DailyChart({ data }: { data?: Leader[] }) {
         </h2>
       </div>
 
-      <div className="w-full h-full min-h-[340px] px-4">        <ResponsiveContainer width="100%" height="100%">
+      <div className="w-full h-[340px] px-4">        
+          <ResponsiveContainer width="100%" height="100%">
           <BarChart data={chartData}>
             <XAxis
               dataKey="name"
