0ec7185e1d72cb3f335bacbae393fc6e43d280fa
Aditya Raut <adityarautz@gmail.com>
2026-05-06 01:40:10 +0530

    fix: proxy IPL

--- app/components/OverallChart.tsx
+++ app/components/OverallChart.tsx
@@ -168,7 +168,7 @@ export default function OverallChart({ data }: { data?: Leader[] }) {
         fontSize={isMobile ? 9 : 12}
         fontWeight={600}
         textAnchor="middle"
-        transform={`rotate(-40, ${cx}, ${cy})`}
+        transform={`rotate(-30, ${cx}, ${cy})`}
       >
         {formatNumber(Number(value))}
       </text>
